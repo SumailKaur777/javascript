6e5adad38e6a9d2ad8eed6f00a6ea94ef7594a04
SumailKaur <155574814+SumailKaur@users.noreply.github.com>
2024-05-02 14:48:39 -0500

    first commit

---
+++ sandbox.js
@@ -0,0 +1,58 @@
+/*let age = 25;
+let year = 2001;
+console.log(age, year);
+
+age = 23;
+console.log(age);
+
+const points = 100;
+
+console.log(points);
+
+var score = 33;
+console.log(score);
+
+let email = 'skaur@thenetninja.co.ca';
+console.log(email);
+
+let firstName = 'Sumail';
+let lastName = 'Pannu';
+
+let fullName = firstName + lastName;
+console.log(fullName);
+
+console.log(fullName[0]);
+
+console.log(fullName.length);
+
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result);
+
+let index = email.indexOf('@');
+console.log(index);
+let email = 'skaur@thenetninja.co.ca';
+
+//let result = email.lastIndexOf('n');
+//let result = email.slice(0,10);
+
+//let result = email.substr(4,10);
+// let result = email.replace('s','p');
+
+
+console.log(result);*/
+
+
+
+let radius = 10;
+const pi = 3.14;
+
+
+//console.log(radius, pi);
+//console.log(10/2);
+//let result = pi* radius**2;
+
+// order of operation - B I D M A S
+
+let result = 5* (10-3)**2;
+console.log(result);
